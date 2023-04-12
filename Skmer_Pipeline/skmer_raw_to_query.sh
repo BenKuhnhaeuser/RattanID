ecd97f72509b6a5d9be4c09caf66b36e5760df98
Benedikt Kuhnhäuser <56020162+BenKuhnhaeuser@users.noreply.github.com>
2023-04-12 16:08:05 +0100

    Update skmer_raw_to_query.sh

--- Skmer_Pipeline/skmer_raw_to_query.sh
+++ Skmer_Pipeline/skmer_raw_to_query.sh
@@ -28,11 +28,10 @@ skmer_db=./skmer_reference_db/
 # Query data (NEED TO SPECIFY FILE AND DIRECTORY LOCATIONS AND FILE ENDINGS)
 #------------
 # Raw data directory
-## needs to contain paired end reads
 data_directory=./data/
 
 # File ending
-## Common ending of forward read, excluding sample name
+## Common ending of forward reads, excluding sample name
 file_ending="_S1_L005_R1_001.fastq.gz"
 
 # Sequence name list
@@ -115,7 +114,7 @@ mv "$name_sample"_summary_tmp.txt "$name_sample"_summary.txt
 
 
 #-----------------------------
-# Clean up intermediate files
+# Clean up intermediate files (DEACTIVATE COMMANDS USING '#' IF WANTING TO KEEP FILES)
 #-----------------------------
 # Remove trimmed reads
 rm "$name_sample"_{1,2}{U,P}.fastq.gz
